470a5f3cdc0c365a225ff0c999ac2d11a66f9297
erika fonseca <fonsecarika@gmail.com>
2024-06-03 04:09:17 +0300

    line 5 deleted

--- Code.js
+++ Code.js
@@ -2,7 +2,6 @@ function apikey() {
   return PropertiesService.getScriptProperties().getProperty('API_KEY');
 }
 
-
 function findEmail(first_name, last_name, domain) {
 
   const url = `https://api.hunter.io/v2/email-finder?domain=${domain}&first_name=${first_name}&last_name=${last_name}&api_key=${apikey()}`
